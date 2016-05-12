dee0ef3e1c36636223a6071cfd2c75c7f472298f
HoangDH <hoang89yenthe@gmail.com>
2016-05-12 22:43:48 +0700

    Add base calculator to github

---
+++ app/src/main/java/fsoft/hoangdh/basicalculator/MainActivity.java
@@ -0,0 +1,118 @@
+package fsoft.hoangdh.basicalculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    TextView tvResult;
+
+    String oper = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // find the elements
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        // set a listener
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        float num1;
+        float num2;
+        float result = 0;
+
+        // check if the fields are empty
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())) {
+            return;
+        }
+
+        // read EditText and fill variables with numbers
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+        // defines the button that has been clicked and performs the corresponding operation
+        // write operation into oper, we will use it later for output
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+
+        // form the output line
+        tvResult.setText(String.format("%s %s %s = %s", num1, oper, num2, result));
+    }
+}
+
